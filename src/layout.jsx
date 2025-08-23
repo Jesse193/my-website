4e3fd8330fe71e5dc88ad728f34973f605b079a1
Jesse Sorman <jessesorman@gmail.com>
2025-08-22 21:59:51 -0400

    add layout navbar

---
+++ src/layout.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { NavLink, Outlet } from "react-router-dom";
+import { HashRouter, Route, Routes, Link } from "react-router-dom";
+import './App.css'
+
+const Layout = ()=> {
+  return (
+    <div>
+      <header>
+        <nav className="bg-gray-800 mb-0 h-10 mt-10 mb-0 w-full inline-flex items-center text-white">
+          <ul>
+            <li className="space-x-20 ml-20 text-2xl">
+              <NavLink to="/" end>Home</NavLink>
+              <NavLink to="/portfolio" end>Portfolio</NavLink>
+            </li>
+          </ul>
+          <a className="inline-flex items-center ml-220 mr-10" target="_blank" rel="noopener" href="https://github.com/Jesse193">
+            <img className="mx-auto block h-15 rounded-full sm:mx-0 sm:shrink-0" src="src/assets/images/github-mark-white.png" alt="Github" />
+          </a>
+          <a className="inline-flex items-center" target="_blank" rel="noopener" href="https://www.linkedin.com/in/jesse-sorman/">
+            <img className="block h-15" src="src/assets/images/LI-In-Bug.png" alt="Linkedin" />
+          </a>
+        </nav>
+      </header>
+      <Outlet />
+    </div>
+  );
+}
+export default Layout
