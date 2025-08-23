b9c28596a668b7c6c024014c3e4c267cbc60ec4e
Jesse Sorman <jessesorman@gmail.com>
2025-08-22 21:17:19 -0400

    add routing for portfolio page

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Portfolio from "./portfolio.jsx"
 import { HashRouter, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/portfolio" element={<Portfolio />} />
     </Routes>
   </HashRouter>
 )
