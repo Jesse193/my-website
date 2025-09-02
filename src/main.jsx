8776963c855ad34fb1805fb9a8d83a9185ea5257
Jesse Sorman <jessesorman@gmail.com>
2025-09-02 12:58:12 -0400

    add 404 error handling

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Portfolio from "./portfolio.jsx"
-import Layout from './layout.jsx'
+import Layout from "./layout.jsx"
+import NotFound from "./NotFound.jsx"
 import { HashRouter, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
       <Route element={<Layout />}>
         <Route path="/" element={<App />} />
         <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="*" element={<NotFound/>} />
       </Route>
     </Routes>
   </HashRouter>
