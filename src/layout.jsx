8a70357d5e7b56d475ae24d31becad2f1c96e147
Jesse Sorman <jessesorman@gmail.com>
2026-01-20 14:39:56 -0500

    add large screen nav bar compatibility

--- src/layout.jsx
+++ src/layout.jsx
@@ -7,8 +7,8 @@ const Layout = ()=> {
   return (
     <div>
       <header>
-        <nav className="relative bg-gray-800 text-white flex flex-wrap justify-between w-screen h-20 md:h-20">
-          <div className="text-xl md:text-2xl absolute flex flex-wrap items-center justify-start left-1/25 top-1/2 space-x-10">
+        <nav className="relative bg-gray-800 text-white flex flex-wrap justify-between w-screen h-20 md:h-20 lg:h-30">
+          <div className="text-xl md:text-2xl lg:text-3xl absolute flex flex-wrap items-center justify-start left-1/25 top-1/2 space-x-10 lg:space-x-15">
             <a>
               <NavLink to="/" end>Home</NavLink>
             </a>
@@ -16,12 +16,12 @@ const Layout = ()=> {
               <NavLink to="/portfolio" end>Portfolio</NavLink>
             </a>
           </div>
-          <div className="absolute flex flex-wrap items-center justify-end right-1/25 top-1/3 space-x-5 md:space-x-15">
+          <div className="absolute flex flex-wrap items-center justify-end right-1/25 top-1/3 space-x-5 md:space-x-15 lg:space-x-15">
             <a target="_blank" rel="noopener" href="https://github.com/Jesse193">
-              <img className="h-15 object-center rounded-full" src="images/github-mark-white.png" alt="Github" />
+              <img className="h-15 lg:h-20 object-center rounded-full" src="images/github-mark-white.png" alt="Github" />
             </a>
             <a target="_blank" rel="noopener" href="https://www.linkedin.com/in/jesse-sorman/">
-              <img className="h-15" src="images/LI-In-Bug.png" alt="Linkedin" />
+              <img className="h-15 lg:h-20" src="images/LI-In-Bug.png" alt="Linkedin" />
             </a>
           </div>
         </nav>
