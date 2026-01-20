292a06cbc4a8f075596efb5fcbdee7c6485a96a1
Jesse Sorman <jessesorman@gmail.com>
2026-01-20 14:35:32 -0500

    add nav bar compatibility for mobile horizontal

--- src/layout.jsx
+++ src/layout.jsx
@@ -7,8 +7,8 @@ const Layout = ()=> {
   return (
     <div>
       <header>
-        <nav className="relative bg-gray-800 text-white flex flex-wrap justify-between w-screen h-20">
-          <div className="text-xl absolute flex flex-wrap items-center justify-start left-1/25 top-1/2 space-x-10">
+        <nav className="relative bg-gray-800 text-white flex flex-wrap justify-between w-screen h-20 md:h-20">
+          <div className="text-xl md:text-2xl absolute flex flex-wrap items-center justify-start left-1/25 top-1/2 space-x-10">
             <a>
               <NavLink to="/" end>Home</NavLink>
             </a>
@@ -16,7 +16,7 @@ const Layout = ()=> {
               <NavLink to="/portfolio" end>Portfolio</NavLink>
             </a>
           </div>
-          <div className="absolute flex flex-wrap items-center justify-end right-1/25 top-1/3 space-x-5">
+          <div className="absolute flex flex-wrap items-center justify-end right-1/25 top-1/3 space-x-5 md:space-x-15">
             <a target="_blank" rel="noopener" href="https://github.com/Jesse193">
               <img className="h-15 object-center rounded-full" src="images/github-mark-white.png" alt="Github" />
             </a>
